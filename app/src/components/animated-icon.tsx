548c7fe1897af7d109f08d707755c2a89e09c41f
Akiva Hershkowitz <67554860+upsitesolutions@users.noreply.github.com>
2026-06-04 15:29:26 -0400

    feat: Update splash screen background color and image for improved branding

--- app/src/components/animated-icon.tsx
+++ app/src/components/animated-icon.tsx
@@ -126,7 +126,7 @@ const styles = StyleSheet.create({
   },
   backgroundSolidColor: {
     ...StyleSheet.absoluteFill,
-    backgroundColor: '#208AEF',
+    backgroundColor: '#ffffff',
     zIndex: 1000,
   },
 });
